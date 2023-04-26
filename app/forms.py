1a0c809d4af73744678e35e32a1ecd24e9242d3f
harshadvali11 <harshadvali1431@gmail.com>
2023-04-26 17:39:19 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.core import validators
 def check_for_a(value):
     if value[0].lower()=='a':
         raise forms.ValidationError('name started with a')
@@ -9,12 +9,12 @@ def check_for_len(value):
         raise forms.ValidationError('len is less')
 
 class StudentForm(forms.Form):
-    name=forms.CharField(max_length=100,validators=[check_for_a,check_for_len])
+    name=forms.CharField(max_length=100,validators=[check_for_a,validators.MaxLengthValidator(5)])
     age=forms.IntegerField()
     email=forms.EmailField()
     re_enter_email=forms.EmailField()
     botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
-
+    mobile=forms.CharField(max_length=10,min_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
     def clean(self):
         e=self.cleaned_data['email']
         r=self.cleaned_data['re_enter_email']
